1bd5cc386804b22b0cea6de6ddc02746a60744c2
DanieldeAlcala <74731526+DanieldeAlcala@users.noreply.github.com>
2026-02-12 09:19:55 +0000

    Deploying to gh-pages from @ DanieldeAlcala/DanielDeAlcala.github.io@bb9a0eb7070df9e6baeedd5900a1fda99e29a63b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,17 +25,10 @@ ninja.data = [{
           },
         },{id: "nav-repositories",
           title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          description: "A collection of research frameworks, deep learning experiments, and code that actually does what it’s told.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
           title: "teaching",
